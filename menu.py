890a375572a4f5135f0340f91846d52f1fd4801a
epilef18 <f.villasanles@gmail.com>
2024-08-20 23:08:57 -0400

    primer commit, menú terminado

---
+++ menu.py
@@ -0,0 +1,20 @@
+def mostrar_menu():
+    print(r"   _____ _                     _      _______   ")
+    print(r"  |  __ (_)                   | |  /\|__   __|  ")
+    print(r"  | |__) | __________ _       | | /  \  | |     ")
+    print(r"  |  ___/ |_  /_  / _` |  _   | |/ /\ \ | |     ")
+    print(r"  | |   | |/ / / / (_| | | |__| / ____ \| |     ")
+    print(r"  |_|   |_/___/___\__,_|  \____/_/    \_\_|     ")
+    print("------------BIENVENIDO A PIZZA JAT---------------")
+    print("Por favor, seleccione una opción:")
+    print("1. Seleccionar masa")
+    print("2. Seleccionar salsa")
+    print("3. Seleccionar ingredientes")
+    print("4  Mostrar ingredientes seleccionados")
+    print("5. Tiempo de entrega y confirmar Pedido")
+    print("6. Salir")
+    return int(input("¿Qué opcion desea?:"))
+
+    
+                                             
+                                             
